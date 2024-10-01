4de4dbf6288f4b9035cb3797b0b7b6188f048361
Angel Miranda <pixeldev1729@gmail.com>
2024-10-01 11:21:27 -0600

    refactor!: remove ddd folder

--- ddd/domain.py
+++
@@ -1,7 +0,0 @@
-class Agente:
-    """Entidad que representa a un agente con su posición y costo acumulado."""
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.costo_total = 0
